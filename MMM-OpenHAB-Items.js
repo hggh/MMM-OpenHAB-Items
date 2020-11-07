2a90ed13c1de9408e4b3ec835a6079aac56f459d
Jonas Genannt <jonas@brachium-system.net>
2020-11-07 21:48:43 +0100

    add Version with suport for switch (light) and rollershutter

---
+++ MMM-OpenHAB-Items.js
@@ -0,0 +1,131 @@
+
+Module.register('MMM-OpenHAB-Items', {
+  items: [],
+  config: null,
+
+  defaults: {
+    tableClass: 'xsmall',
+  },
+
+  getStyles: function() {
+    return ['MMM-OpenHAB-Items.css', 'font-awesome.css'];
+  },
+
+  start: function() {
+    Log.info("Starting module: " + this.name);
+    if (this.config.url == null) {
+      Log.error("MMM-OpenHAB-Items.js: url parameter in config not found.");
+      return;
+    }
+    for (var i in this.config.items) {
+      var item = this.config.items[i];
+      this.addItem(this.config.url, item);
+      
+    }
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === 'NEW_ITEM') {
+      this.items.push({
+        item_label: payload.item_label,
+        item_name: payload.item_name,
+        icon: payload.icon,
+        item_type: payload.item_type,
+        item_value: payload.item_value,
+      })
+    }
+
+    this.updateDom(200);
+  },
+
+  addItem: function(url, item) {
+    this.sendSocketNotification("ADD_ITEM", {
+      url: url,
+      item_name: item.item_name,
+      icon: item.icon,
+    });
+  },
+
+  getHeader: function() {
+    return "OpenHAB";
+  },
+
+  getDom: function() {
+    var table = document.createElement("table");
+    table.className = this.config.tableClass;
+
+    if (this.items.length == 0) {
+      var noDataRow = document.createElement("tr");
+      noDataRow.className = "normal";
+      var noDataCell = document.createElement("td");
+      noDataCell.innerHTML = "no data";
+      noDataRow.appendChild(noDataCell);
+      table.appendChild(noDataRow);
+
+      return table;
+    }
+
+    for (var i in this.items) {
+      var self = this;
+      var item = this.items[i];
+      var dataRow = document.createElement("tr");
+      var labelCell = document.createElement("td");
+      var valueCell = document.createElement("td");
+      labelCell.innerHTML = item.item_label;
+      labelCell.className = "label_name";
+
+      item_value = document.createElement("span")
+
+      if (item.item_type == 'Switch') {
+        if (item.icon != null && item.icon == 'light') {
+          item_value.className = "fa fa-fw fa-lightbulb";
+        }
+        else {
+          item_value.className = "fa fa-fw fa-toggle-on";
+        }
+        item_value.setAttribute("id", "MMM-OpenHAB-Items-" + item.item_name);
+        item_value.setAttribute("data-item-name", item.item_name);
+        item_value.addEventListener("click", function() {
+          self.sendSocketNotification("TOGGLE_SWITCH", {item_name: this.dataset.itemName, url: self.config.url})
+        });
+      }
+
+      if (item.item_type == 'Rollershutter') {
+        item_value.className = "Rollershutter";
+        shutter_up = document.createElement("span")
+        shutter_up.setAttribute("data-item-name", item.item_name);
+        shutter_up.className = "fa fa-fw fa-sort-up";
+        shutter_up.addEventListener("click", function() {
+          self.sendSocketNotification("ROLLERSHUTTER", {item_name: this.dataset.itemName, url: self.config.url, action: 'UP'})
+        });
+
+        shutter_halt = document.createElement("span")
+        shutter_halt.setAttribute("data-item-name", item.item_name);
+        shutter_halt.className = "fa fa-fw fa-window-close";
+        shutter_halt.addEventListener("click", function() {
+          self.sendSocketNotification("ROLLERSHUTTER", {item_name: this.dataset.itemName, url: self.config.url, action: 'STOPP'})
+        });
+
+
+        shutter_down = document.createElement("span")
+        shutter_down.setAttribute("data-item-name", item.item_name);
+        shutter_down.className = "fa fa-fw fa-sort-down";
+        shutter_down.addEventListener("click", function() {
+          self.sendSocketNotification("ROLLERSHUTTER", {item_name: this.dataset.itemName, url: self.config.url, action: 'DOWN'})
+        });
+
+        item_value.appendChild(shutter_up);
+        item_value.appendChild(shutter_halt);
+        item_value.appendChild(shutter_down);
+      }
+
+      valueCell.appendChild(item_value);
+
+      dataRow.appendChild(labelCell);
+      dataRow.appendChild(valueCell);
+
+      table.appendChild(dataRow);
+    }
+    return table;
+  },
+});
---
+++ node_helper.js
@@ -0,0 +1,67 @@
+const got = require('got');
+const Log = require("../../js/logger");
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function (notification, payload) {
+    if (notification == 'ADD_ITEM') {
+      this.getItem(payload.url, payload.item_name, payload.icon);
+    }
+    if (notification == 'TOGGLE_SWITCH') {
+      this.toggleSwitch(payload.url, payload.item_name);
+    }
+    if (notification == 'ROLLERSHUTTER') {
+      this.rollershutter(payload.url, payload.item_name, payload.action);
+    }
+  },
+
+  rollershutter: function(url, item_name, action) {
+    got.post(url + item_name, {body: action});
+  },
+
+  toggleSwitch: function(url, item_name) {
+    var item_action = 'OFF';
+    const r = got.get(url + item_name, {responseType: 'json'})
+      .then((response) => {
+         if (response.body.state == 'OFF') {
+           item_action = 'ON';
+         }
+         got.post(url + item_name, {body: item_action });
+    });
+
+  },
+
+  getItem: function(url, item_name, icon) {
+    const r = got.get(url + item_name, {responseType: 'json'})
+      .then((response) => {
+        item_label = response.body.label;
+       
+        item_type = null;
+        item_value = null;
+
+        if (response.body.type == 'Switch') {
+          item_type = 'Switch';
+        }
+        if (response.body.type == 'Rollershutter') {
+          item_type = 'Rollershutter';
+        }
+
+        if (item_type != null) {
+          this.sendSocketNotification("NEW_ITEM", {
+            item_label: item_label,
+            item_name: item_name,
+            item_type: item_type,
+            icon: icon,
+            item_value: item_value,
+          });
+        }
+        else {
+          Log.warn("Item " + item_name + " with Type " + response.body.type + " not supported");
+        }
+    });
+  },
+});
+
+
+
